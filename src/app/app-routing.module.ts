4912e5e015bf51ab230c42a9377c8e93837faa08
zakaria <zak69164@gmail.com>
2024-01-06 20:22:08 +0100

    Setup some Modules and Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,17 +4,21 @@ import { LoginComponent } from './auth/login/login.component';
 import { NewUserComponent } from './auth/new-user/new-user.component';
 import { ForgotComponent } from './auth/forgot/forgot.component';
 import { LogoutComponent } from './auth/logout/logout.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent},
   { path: 'register', component: NewUserComponent},
   { path: 'forgot-password', component: ForgotComponent},
   { path: 'logout', component: LogoutComponent},
+  { path: 'dashboard', component: DashboardComponent},
   { path: 'customers', loadChildren: () => import('./customers/customers.module').then(m => m.CustomersModule) }, 
   { path: 'loans', loadChildren: () => import('./loans/loans.module').then(m => m.LoansModule) }, 
   { path: 'invoices', loadChildren: () => import('./invoices/invoices.module').then(m => m.InvoicesModule) }, 
   { path: 'payments', loadChildren: () => import('./payments/payments.module').then(m => m.PaymentsModule) }, 
-  { path: 'settings', loadChildren: () => import('./settings/settings.module').then(m => m.SettingsModule) }
+  { path: 'settings', loadChildren: () => import('./settings/settings.module').then(m => m.SettingsModule) },
+  { path: '**', component: PageNotFoundComponent}
 ];
 
 @NgModule({
