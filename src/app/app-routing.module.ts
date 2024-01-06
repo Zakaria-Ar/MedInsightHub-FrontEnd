16690bb5f1e29738c046586de3d18c55df7db301
zakaria <zak69164@gmail.com>
2024-01-06 19:56:34 +0100

    Routing et ajouter Modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { NewUserComponent } from './auth/new-user/new-user.component';
+import { ForgotComponent } from './auth/forgot/forgot.component';
+import { LogoutComponent } from './auth/logout/logout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent},
+  { path: 'register', component: NewUserComponent},
+  { path: 'forgot-password', component: ForgotComponent},
+  { path: 'logout', component: LogoutComponent},
+  { path: 'customers', loadChildren: () => import('./customers/customers.module').then(m => m.CustomersModule) }, 
+  { path: 'loans', loadChildren: () => import('./loans/loans.module').then(m => m.LoansModule) }, 
+  { path: 'invoices', loadChildren: () => import('./invoices/invoices.module').then(m => m.InvoicesModule) }, 
+  { path: 'payments', loadChildren: () => import('./payments/payments.module').then(m => m.PaymentsModule) }, 
+  { path: 'settings', loadChildren: () => import('./settings/settings.module').then(m => m.SettingsModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
