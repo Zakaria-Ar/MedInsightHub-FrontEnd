d71d5d9f20c082bff7d9e1dcc942319ca53f970f
zakaria <zak69164@gmail.com>
2024-01-07 10:29:29 +0100

    Integrating Customer Service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { SidebarComponent } from './sidebar/sidebar.component';
 import { FooterComponent } from './footer/footer.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/services/customer.service.ts
+++ src/app/services/customer.service.ts
@@ -1,11 +1,30 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment.dev';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CustomerService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
 
-  //Methods to communicate with our backend APIs
+  getCustomers(){
+    let url = environment.CUSTOMER_BASE_URL+environment.CUSTOMER.GET_ALL_CUSTOMERS;
+    return this.httpClient.get(url);
+
+  }
+  viewCustomer(id: any){
+
+  }
+  editCustomer(id: any, _customerObj: any){
+
+  }
+  deleteCustomer(id: any){
+
+  }
+  searchCustomer(keyword: any){
+
+  }
+
 }
