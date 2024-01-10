33a9a6dea330a9e7ad0cf5c9dd1f9cb494638664
zakaria <zak69164@gmail.com>
2024-01-10 01:55:49 +0100

    Login and registration updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, Event, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -7,9 +8,18 @@ import { Router } from '@angular/router';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  constructor(public router: Router) { }
+  showHeaderAndSidebar: boolean = true;
   title = 'admin-panel-layout';
   sideBarOpen = true;
+
+  constructor(private router: Router) {
+    // Subscribe to router events and determine whether to show the header and sidebar
+    this.router.events.pipe(
+      filter((event: Event): event is NavigationEnd => event instanceof NavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+      this.showHeaderAndSidebar = !(event.url === '/login' || event.url === '/' || event.url === '/register');
+    });
+  }
 
   sideBarToggler() {
     this.sideBarOpen = !this.sideBarOpen;
