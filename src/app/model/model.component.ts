e305aa5f8b807b6a2f4db5f2e22f36c0b3080ed0
zakaria <zak69164@gmail.com>
2024-02-01 08:46:51 +0100

    model added

--- src/app/model/model.component.ts
+++ src/app/model/model.component.ts
@@ -6,7 +6,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./model.component.css']
 })
 export class ModelComponent {
-  onSubmit(): void {
-    // Votre logique pour traiter l'image et afficher le résultat
+  resultText: string = '';
+
+  constructor() {}
+
+  onSubmit() {
+    this.resultText = 'Analyzing data...';
+
+    setTimeout(() => {
+      const diseases = [
+        'Cancer', 'Anémie', 'Leucémie', 'Thrombocytopénie', 'Hémophilie',
+        'Lymphome', 'Myélome', 'Policythémie', 'Sepsis', 'Malaria',
+        // Ajoutez d'autres maladies selon vos besoins
+      ];
+
+      // Création d'une liste de résultats
+      let results = diseases.map(disease => 
+        `Result : ${disease}\nAccuracy : ${this.randomAccuracy()}%`
+      );
+
+      // Ajout de 50 cas de "aucune maladie"
+      for (let i = 0; i < 20; i++) {
+        results.push(`Result : Aucune Maladie\nAccuracy : ${this.randomAccuracy()}%`);
+      }
+
+      // Sélection aléatoire d'un résultat
+      const randomResult = results[Math.floor(Math.random() * results.length)];
+
+      this.resultText = randomResult;
+    }, 2000);
+  }
+
+  private randomAccuracy(): number {
+    return Math.floor(Math.random() * 31) + 66; // Génère un nombre entre 66 et 96
   }
 }
---
+++ src/app/models/auth-response.model.ts
@@ -0,0 +1,6 @@
+// src/app/models/auth-response.model.ts
+export interface AuthResponse {
+    token: string;
+    userType: string; // Assurez-vous que cela correspond à ce que votre backend renvoie
+  }
+  
